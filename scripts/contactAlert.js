eb5a331f156fcfe205d74da722e82f148b56fc92
m3r71n <54747628+m3r71n@users.noreply.github.com>
2020-11-02 20:30:36 -0500

    formatting changes

--- scripts/contactAlert.js
+++ scripts/contactAlert.js
@@ -2,5 +2,5 @@ const firstName = document.getElementById('firstName')
 const form = document.getElementById('ContactForm')
 
 function onSubmitFunction(event) {
-  alert(firstName.value + ' Thanks for the message!')
+  alert(firstName.value + ', thanks for the message!')
 }
